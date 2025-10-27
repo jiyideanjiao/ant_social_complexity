4d63be2841fd25af2f79413c6fc172304bd1a1b0
Chao Tong <33243479+jiyideanjiao@users.noreply.github.com>
2025-10-27 10:49:10 -0400

    Update and rename run_BUSCO.sh to Genome_Annotation/run_BUSCO.sh

--- Genome_Annotation/run_BUSCO.sh
+++ Genome_Annotation/run_BUSCO.sh
@@ -1,27 +1,27 @@
 ## genome - assembly
 busco -i {genome.fas} \
-        -l ./arachnida_odb10 \
+        -l ./hymenoptera_odb10 \
         -o busco \
         -m genome \
         --cpu 30
 
 ## genome - annotated gene models
 busco -i {genome.pep} \
-        -l ./arachnida_odb10 \
+        -l ./hymenoptera_odb10 \
         -o busco \
         -m prot \
         --cpu 30
 
 ## transcriptome - assembly
 busco -i {RNA.fas} \
-        -l ./arachnida_odb10 \
+        -l ./hymenoptera_odb10 \
         -o busco \
         -m trans \
         --cpu 30
         
 ## transcriptome - annotated gene models
 busco -i {RNA.fas} \
-        -l ./arachnida_odb10 \
+        -l ./hymenoptera_odb10 \
         -o busco \
         -m prot \
         --cpu 30
